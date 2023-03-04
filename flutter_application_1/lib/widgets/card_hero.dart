ebc878b0db995d1d1d9b17721cae01acd15485cc
WorkerStudy <126880778+krevetkoss@users.noreply.github.com>
2023-03-04 17:11:11 +0600

    1 лаб. работа

---
+++ flutter_application_1/lib/widgets/card_hero.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../constans.dart';
+import '../models/heroes.dart';
+
+class CardHero extends StatelessWidget {
+  Heroes _hero;
+
+  CardHero(this._hero);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Card(
+            elevation: 0.6,
+            clipBehavior: Clip.hardEdge,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: Stack(fit: StackFit.expand, children: [
+              SizedBox.expand(
+                  child: Image.asset(
+                _hero.pathImage,
+                fit: BoxFit.cover,
+              )),
+              Positioned(
+                  bottom: 10,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      _hero.name,
+                      style: titleStyle,
+                    ),
+                  )),
+            ])));
+  }
+}
